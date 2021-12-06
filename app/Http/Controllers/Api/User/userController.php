4a02850f56ceaefc7eb73fdc0c7cc481790ffc6f
phoeni2020 <Ka199484>
2021-12-06 15:56:38 +0200

    Api Changes

--- app/Http/Controllers/Api/User/userController.php
+++ app/Http/Controllers/Api/User/userController.php
@@ -90,7 +90,7 @@ class userController extends Controller
                 $user->city = $data['city'];
                 $user->parentPhone = $data['parentPhone'];
                 $user->save();
-                Log::create(['log' => 'User Completed His data', 'user' => request()->user()->id, 'data' => $user, 'route' => request()->route()->uri()]);
+                Log::create(['log' => 'User Completed His data', 'user' => request()->user()->id, 'data' => json_encode($user), 'route' => request()->route()->uri()]);
 
                 return response()->json(['token'=>$request->header('token'),'user'=>$user,'dataComplete'=>true]);
             }
@@ -136,7 +136,7 @@ class userController extends Controller
             $user->parentPhone = $validatedData['parentPhone'];
             $user->save();
             $data['afterUpdate'] = $user;
-            Log::create(['log' => 'User Data Updated', 'user' => request()->user()->id, 'data' => $data, 'route' => request()->route()->uri()]);
+            Log::create(['log' => 'User Data Updated', 'user' => request()->user()->id, 'data' => json_encode($data), 'route' => request()->route()->uri()]);
 
             return response()->json(['token' => $request->header('token'), 'user' => $user, 'dataComplete' => true]);
         }
@@ -175,7 +175,7 @@ class userController extends Controller
                         User::where('id', $userid)->update(['password' => Hash::make($input['newPassword'])]);
                         $arr = array("status" => 200, "message" => "Password updated successfully.",
                             "data" => array('user' => $request->user(), 'dataComplete' => true, 'token' => $request->header('token')));
-                        Log::create(['log' => 'User Updated Password', 'user' => request()->user()->id, 'data' => [], 'route' => request()->route()->uri()]);
+                        Log::create(['log' => 'User Updated Password', 'user' => request()->user()->id, 'data' => '', 'route' => request()->route()->uri()]);
 
                     }
                 }
