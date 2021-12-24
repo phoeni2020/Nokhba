79a09170e891d19c41718ee6880a132c32509d81
phoeni2020 <Ka199484>
2021-12-24 17:27:50 +0200

    Test FCM

---
+++ app/Http/Controllers/Api/testingAPI.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class testingAPI extends Controller
+{
+    function sendGCM($token)
+    {
+        // FCM API Url
+        $url = 'https://fcm.googleapis.com/fcm/send';
+
+        // Put your Server Response Key here
+        $apiKey = 'AAAA3Kma2Uw:APA91bHPeZaDv5Ku0S8F_gpXvtiR3F7RC5imlbx0cR22tp7OfVcz6DaQWY_2-dphE7JvRjEIrbko07lyC_97BQi64tSjsjWu1IvuoPnaUai7lvFNIxl4ggaH-45WkOMFWit_79jZ1O5X';
+
+        // Compile headers in one variable
+        $headers = array(
+            'Authorization:key=' . $apiKey,
+            'Content-Type:application/json'
+        );
+
+        // Add notification content to a variable for easy reference
+        $notifData = [
+            'title' => "Test Title",
+            'body' => "Test notification body",
+            'click_action' => "android.intent.action.MAIN"
+        ];
+
+        // Create the api body
+        $apiBody = [
+            'notification' => $notifData,
+            'data' => $notifData,
+            'to' => $token // Replace 'mytargettopic' with your intended notification audience
+        ];
+
+        // Initialize curl with the prepared headers and body
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($apiBody));
+
+        // Execute call and save result
+        $result = curl_exec($ch);
+
+        // Close curl after call
+        curl_close($ch);
+
+        return $result;
+    }
+}
